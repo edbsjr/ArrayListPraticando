7fb9df77c2b9799e45893eeaf9fbb8a0f8b98082
edbsjr <eduardobsjunior@hotmail.com>
2025-02-24 02:25:18 +0000

    Add files via upload

--- ListaDeTarefas/src/br/com/tarefas/Main.java
+++ ListaDeTarefas/src/br/com/tarefas/Main.java
@@ -1,79 +1,119 @@
 package br.com.tarefas;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
+		ArrayList<Tarefa> listaDeTarefas = new ArrayList<>();
 
-		Tarefa listaDeTarefas = new Tarefa();
-
-		int operador;
-		int indice = 0;
-		String tarefa;
+		int operador = 1;
 
 		System.out.println("---Bem Vindo a sua lista de Tarefas---");
-		System.out.println("--------------------------------------");
-		System.out.println("Escolha uma opção:");
-		System.out.println("1-Adicionar Tarefa");
-		System.out.println("2-Remover Tarefa");
-		System.out.println("3-Lista Completa(Ordem Cronologica)");
-		System.out.println("4-Lista Completa(Ordem Alfabetica");
-		System.out.println("5-SAIR");
-		System.out.println("Digite agora:");
-		operador = input.nextInt();
-
-		while (operador != 5) {
-			switch (operador) {
-			case 1:
-				System.out.println("Digite qual tarefa deseja adicionar:");
-				input = new Scanner(System.in);
-				tarefa = input.nextLine();
-				listaDeTarefas.adicionar(tarefa); // FEITO: CONSULTA A LISTA E DEPOIS ADICIONAR A LISTA
-				break;
-			case 2:
-				System.out.println("Voce quer remover pelo INDICE(1) ou pela DESCRIÇÃO(2):");
-				input = new Scanner(System.in);
-				int remov = input.nextInt();
-				if (remov == 2) {
-					System.out.println("Digite qual tarefa deseja remover:");
-					input = new Scanner(System.in);
-					tarefa = input.nextLine();
-					listaDeTarefas.removerDescricao(tarefa);// FEITO: CHAMA PARA REMOVER POR DESCRIÇÃO
-				} else if (remov == 1) {
-					System.out.println("Digite o INDICE deseja remover:");
-					input = new Scanner(System.in);
-					indice = input.nextInt();
-					listaDeTarefas.removerIndice(indice); // FEITO: CHAMA PARA REMOVER PELO INDICE
-
-				}
-				break;
-			case 3:
-				listaDeTarefas.imprimirCronos();
-				break;
-			case 4:
-				//FALTA IMPLEMENTAR A ORDEM ALFABETICA
-				break;
-			case 5:
-				System.out.println("---------PROGRAMA FINALIZADO----------");
-			}
-			if (operador == 5) {
-				break;
-			}
+		
+		
+		while (operador != 0) {
 
 			System.out.println("--------------------------------------");
 			System.out.println("Escolha uma opção:");
 			System.out.println("1-Adicionar Tarefa");
 			System.out.println("2-Remover Tarefa");
-			System.out.println("3-Lista Completa(Ordem Cronologica)");
+			System.out.println("3-Lista Completa");
 			System.out.println("4-Lista Completa(Ordem Alfabetica");
-			System.out.println("5-SAIR");
+			System.out.println("5-Lista Completa(Ordem Cronologica de Execucao");
+			System.out.println("0-SAIR");
 			System.out.println("Digite agora:");
-			input = new Scanner(System.in);
 			operador = input.nextInt();
+
+			switch (operador) {
+			case 1:
+				System.out.println("Digite o Titulo gostaria de adicionar:");
+				input = new Scanner(System.in);
+				String titulo = input.nextLine();
+				System.out.println("Digite a descrição gostaria de adicionar:");
+				String descricao = input.nextLine();
+				System.out.println("Digite qual data sera realizada:(YYYYMMDD)");
+				int data = input.nextInt();
+
+				Tarefa tarefa = new Tarefa(titulo, descricao, data);
+				boolean existe = false;
+				
+				for (int i = 0; i < listaDeTarefas.size(); i++) {
+
+					if (titulo.equals(listaDeTarefas.get(i).getTitulo())) { // FUNCIONA QUANDO PROCURO TITULO // NAO
+																			// FUNCIONOU COMPARANDO O OBJETO COMPLETO
+						System.out.println("Já existe essa tarefa registrada.");
+						existe = true;
+					}
+				}
+				if (existe == false) {
+					if (listaDeTarefas.add(tarefa))
+						System.out.println("Tarefa adicionada com sucesso.");
+					else
+						System.out.println("Erro ao adicionar a Tarefa");
+				}
+
+				break;
+			case 2:
+
+				System.out.println("Voce quer remover pelo INDICE(1) ou pelo TITULO(2):");
+				int remov = input.nextInt();
+
+				if (remov == 2) {
+					System.out.println("Digite qual titulo deseja remover:");
+					input = new Scanner(System.in);
+					titulo = input.nextLine();
+
+					for (int i = 0; i < listaDeTarefas.size(); i++) {
+						if (titulo.equalsIgnoreCase(listaDeTarefas.get(i).getTitulo())) {
+							listaDeTarefas.remove(listaDeTarefas.get(i));
+						}
+					}
+
+				} else if (remov == 1) {
+					System.out.println("Digite o INDICE deseja remover:");
+					int indice = input.nextInt();// PRECISA SER COMPLETADO A APRESENTACAO DA TABELA PARA MELHOR
+													// VISUALIZACAO
+					listaDeTarefas.remove(indice);
+				}
+
+				break;
+			case 3:
+
+				for (Tarefa t : listaDeTarefas) {
+					System.out.println(t.getTitulo() + " - " + t.getDescricao() + " | Data: " + t.getData());
+				}
+				break;
+			case 4:
+				//listaDeTarefas.sort(Comparator.comparing(data)); // AINDA TENTANDO ENTENDER COMO ORDERNAR
+				
+				break;
+			case 0:
+				System.out.println("---------PROGRAMA FINALIZADO----------");
+				break;
+			default:
+				System.out.println("Opção Invalida");
+				
+				titulo = "Primeira"; descricao="primeira"; data= 2025;
+				Tarefa tarefa2 = new Tarefa(titulo, descricao, data);
+				listaDeTarefas.add(tarefa2);
+				titulo = "Segunda"; descricao="segunda"; data= 2024;
+				Tarefa tarefa3 = new Tarefa(titulo, descricao, data);
+				listaDeTarefas.add(tarefa3);
+				titulo = "Terceira"; descricao="terceira"; data= 2023;
+				Tarefa tarefa4 = new Tarefa(titulo, descricao, data);
+				listaDeTarefas.add(tarefa4);
+				titulo = "Quarta"; descricao="quarta"; data= 2022;
+				Tarefa tarefa5 = new Tarefa(titulo, descricao, data);
+				listaDeTarefas.add(tarefa5);
+				break;
+			}
+
 		}
 
 	}
--- ListaDeTarefas/src/br/com/tarefas/Tarefa.java
+++ ListaDeTarefas/src/br/com/tarefas/Tarefa.java
@@ -5,74 +5,39 @@ import java.util.ArrayList;
 
 public class Tarefa {
 
-	private ArrayList<String> listaDeTarefas = new ArrayList<>();
-	// private ArrayList<Integer> listaDeIndices = new ArrayList<>();
-	private boolean confirmado = true;
-	private String tarefa;
-	private int indice;
+	private String titulo;
+	private String descricao;
+	private int data;
 
-	public void adicionar(String tarefa) {
-
-		this.tarefa = tarefa;
-		confirmado = true;// Checando se a propriedade esta conforme deve
-
-		for (int i = 0; i < listaDeTarefas.size(); i++) { // CHECA TODAS AS LINHAS DA LISTA
-
-			if (tarefa == listaDeTarefas.get(i)) {
-				confirmado = false;
-				System.out.println("Item ja adicionado.");
-				break; // Ou Continue
-			}
-
-		}
-		if (confirmado == true) {
-
-			listaDeTarefas.addLast(tarefa); // ADICIONA NA ULTIMA POSIÇÃO GARANTINDO A CRONOLOGIA
-			System.out.println("Item adicionado com Sucesso.");
-		}
-	}
-
-	public void removerDescricao(String tarefa) {
-		
-		confirmado = false;
-		
-		for (int i = 0; i < listaDeTarefas.size(); i++) {
-
-			if (tarefa == listaDeTarefas.get(i)) {
-				confirmado = true;
-				listaDeTarefas.remove(i);
-				System.out.println("Item removido com sucesso.");
-				break; // Ou Continue para interromper o FOR
-			}
-		}
-		if(confirmado == false)
-			System.out.println("Item não encontrado para remover.");
-
-	}
-	
-	public void removerIndice(int indice) {
-		this.indice = indice;
-		confirmado = false;
-		
-		tarefa = listaDeTarefas.get(indice); // ESTA PROCURANDO PELA DESCRIÇÃO. DA PRA MELHORAR?
-		confirmado = listaDeTarefas.contains(tarefa);
-		if(confirmado == true) {
-		listaDeTarefas.remove(indice);
-		System.out.println("Indice removido com sucesso.");
-		}
-		else { System.out.println("Indice não encontrado para remoção.");
-			
-		}
-	}
-	
-	public void imprimirCronos()
+	public Tarefa(String titulo, String descricao, int data) // METODO CONSTRUTOR
 	{
-		for (int i = 0; i < listaDeTarefas.size(); i++) {  //IMPRIME TODAS AS TAREFAS POR ORDEM DE ENTRADA
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.data = data;
+	}
 
-			System.out.println("Indice: "+i+" - "+listaDeTarefas.get(i));
-			
-			}
+	public String getTitulo() {
+		return titulo;
+	}
 
-		}
-	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
 }
